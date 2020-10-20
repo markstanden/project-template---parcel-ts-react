df8ed76db0575332eea083ae8afef967ec906c45
Mark Standen <mark@markstanden.uk>
2020-10-20 21:48:41 +0100

    template really fleshed out and ready to use to make some cool apps

---
+++ web/src/components/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App-header">
+        <h1 className="App-header-main">My Super Amazing Mega-App</h1>
+        <h4 className="App-header-sub">maybe...</h4>
+      </div>
+    );
+  }
+}
+
+export default App;
